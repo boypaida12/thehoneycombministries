b43db9d77a9ecc0c99e6241a8d9f06584311cbea
boypaida12 <baddysackey5@gmail.com>
2023-06-13 23:13:44 +0000

    image path fix

--- src/components/gallery/photos.js
+++ src/components/gallery/photos.js
@@ -1,44 +1,36 @@
-// import Gallery_I from "../../assets/Gallery.jpg";
-// import Gallery_II from "../../assets/Gallery_I.jpg";
-// import Gallery_III from "../../assets/Gallery_II.jpg";
-// import Gallery_IV from "../../assets/Gallery_III.jpg";
-// import Gallery_V from "../../assets/Gallery_IV.jpg";
-// import Gallery_VI from "../../assets/Gallery_V.jpg";
-// import Gallery_VII from "../../assets/Gallery_VI.jpg";
-
 export const photos = [
     {
-      src: "/src/assets/Gallery.jpg",
+      src: "./src/assets/Gallery.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "/src/assets/Gallery_II.jpg",
+      src: "./src/assets/Gallery_II.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "/src/assets/Gallery_IV.jpg",
+      src: "./src/assets/Gallery_IV.jpg",
       width: 4,
       height: 4
     },
     {
-      src: "/src/assets/Gallery_V.jpg",
+      src: "./src/assets/Gallery_V.jpg",
       width: 5,
       height: 4
     },
     {
-      src: "/src/assets/Gallery_VI.jpg",
+      src: "./src/assets/Gallery_VI.jpg",
       width: 5,
       height: 4
     },
     {
-      src: "/src/assets/Gallery_III.jpg",
+      src: "./src/assets/Gallery_III.jpg",
       width: 4,
       height: 4
     },
     {
-      src: "/src/assets/Gallery_IX.jpg",
+      src: "./src/assets/Gallery_IX.jpg",
       width: 6,
       height: 4
     },
